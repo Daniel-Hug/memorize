f3b945bbcb26e804222b4a29fb34ed60ec1d5be8
Daniel Hug <danielthehug@gmail.com>
2015-05-30 23:20:34 -0700

    clean up card.js

--- src/views/cards/card.js
+++ src/views/cards/card.js
@@ -20,30 +20,43 @@
 		}
 
 
+
+		// controller functions
+
+		var ctrl = {
+			markCardStudied: function markCardStudied() {
+				this.disabled = true;
+				studyDates.push(Date.now());
+				app.cards.edit(card, {
+					studyDates: studyDates,
+					priority: app.getPriority(studyDates)
+				});
+			},
+			deleteCard: function deleteCard() {
+				app.cards.remove(card);
+			}
+		};
+
+
+
+		var dueMsg =
+			// if studied at all
+			card.studyDates && card.studyDates.length ?
+				// show when it's due for review
+				'due ' + h.daysAgo(-card.priority) :
+				// else
+				'not yet studied';
+
+
+
 		// render li
 		var li = DOM.buildNode({el: 'li', kids: [
 			{ _className: 'controls', kids: [
-				{ el: 'input', type: 'checkbox', _checked: checked, _disabled: checked, on_change: function markCardStudied() {
-					this.disabled = true;
-					studyDates.push(Date.now());
-					app.cards.edit(card, {
-						studyDates: studyDates,
-						priority: app.getPriority(studyDates)
-					});
-					// fix: recalculate card priority
-				} },
-				{ el: 'button', kid: '×', _className: 'delete', _title: 'delete', on_click: function deleteCard() {
-					app.cards.remove(card);
-				} }
+				{ el: 'input', type: 'checkbox', _checked: checked, _disabled: checked, on_change: ctrl.markCardStudied },
+				{ el: 'button', kid: '×', _className: 'delete', _title: 'delete', on_click: ctrl.deleteCard }
 			] },
 			{ _className: 'card-header', kids: [
-				{ el: 'span', _className: 'tag due-msg', kids: [
-					// if studied at all
-					card.studyDates && card.studyDates.length ?
-						// show when it's due for review
-						'due ' + h.daysAgo(-card.priority) :
-						'not yet studied'
-				] }
+				{ el: 'span', _className: 'tag due-msg', kid: dueMsg }
 			] },
 			card.text
 		] });
