a76ef86b40357b67f9dca3033a326092000fe420
Daniel Hug <danielthehug@gmail.com>
2015-06-09 20:20:28 -0700

    make spec/js/app.js pass jscs

--- src/test/spec/js/app.js
+++ src/test/spec/js/app.js
@@ -1,7 +1,6 @@
+/* global describe, it, expect, h, app */
 (function() {
 	'use strict';
-
-	var MS_PER_DAY = 1000 * 60 * 60 * 24;
 
 	describe('getPriority', function() {
 		it('should work when only studied at the start of this morning', function() {
